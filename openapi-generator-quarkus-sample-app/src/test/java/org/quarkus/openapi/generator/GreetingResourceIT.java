56bc25a27432535def706e0462a68bcc8e1bf84c
Berkay Bingol <berkayb@backbase.com>
2022-07-15 14:38:48 +0200

    initial commit for openapi generator with quarkus application

---
+++ openapi-generator-quarkus-sample-app/src/test/java/org/quarkus/openapi/generator/GreetingResourceIT.java
@@ -0,0 +1,9 @@
+package org.quarkus.openapi.generator;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
